1ba805a280fe364b87e01c1f0e3b5d208bb1c508
Nick Brinson <nickbrinson3@gmail.com>
2020-01-09 21:59:36 -0500

    complete implementation of upcoming games

--- basketball/js/sheets-api.js
+++ basketball/js/sheets-api.js
@@ -9,6 +9,9 @@ let todaysYear = this.parseInt(formattedDate[2]);
 const monthNames = ["jan", "feb", "mar", "apr", "may", "june",
     "july", "aug", "sept", "oct", "nov", "dec"
 ];
+const monthNamesFull = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+];
 
 let games = new Array();
 let upcomingGames = new Array();
@@ -56,7 +59,19 @@ function myCallback(spreadsheetdata) {
     }
     console.log(dates);
     console.log(games);
+}
 
+function compare(a, b) {
+    const timeA = this.parseInt(a.time[0]);
+    const timeB = this.parseInt(b.time[0]);
+
+    let comparison = 0;
+    if (timeA > timeB) {
+        comparison = 1;
+    } else if (timeA < timeB) {
+        comparison = -1;
+    }
+    return comparison;
 }
 
 window.onload = function identifyUpcomingGames() {
@@ -70,20 +85,50 @@ window.onload = function identifyUpcomingGames() {
             this.parseInt(monthDayYear[1]) < (todaysDay + 7)) {
             upcomingGames.push(game);
         }
+
+        upcomingGames.sort(compare);
+
     });
-    console.log("diwovneio");
+
+    let upcomingGamesDateSplit = upcomingGames[0].date.split("/");
 
     // Display data from sheet api on index.html
-    document.querySelector("#court-one-teams-six").innerHTML = `${upcomingGames[0].homeTeam} vs. ${upcomingGames[0].awayTeam}`;
-    document.querySelector(".time-slot-six").innerHTML = `${upcomingGames[0].time}`;
-    document.querySelector(".court-one").innerHTML = `CLA - ${upcomingGames[0].court}`;
+    document.querySelector("#upcoming-games-date").innerHTML = `${monthNamesFull[upcomingGamesDateSplit[0] - 1]} ${upcomingGamesDateSplit[1]}, ${upcomingGamesDateSplit[2]}`;
+    document.querySelector("#teams-six-court-a").innerHTML = `${upcomingGames[0].homeTeam} vs. ${upcomingGames[0].awayTeam}`;
+    document.querySelector("#teams-six-court-b").innerHTML = `${upcomingGames[1].homeTeam} vs. ${upcomingGames[1].awayTeam}`;
+    document.querySelector("#teams-seven-court-a").innerHTML = `${upcomingGames[2].homeTeam} vs. ${upcomingGames[2].awayTeam}`;
+    document.querySelector("#teams-seven-court-b").innerHTML = `${upcomingGames[3].homeTeam} vs. ${upcomingGames[3].awayTeam}`;
+    document.querySelector("#teams-eight-court-a").innerHTML = `${upcomingGames[4].homeTeam} vs. ${upcomingGames[4].awayTeam}`;
+    document.querySelector("#teams-eight-court-b").innerHTML = `${upcomingGames[5].homeTeam} vs. ${upcomingGames[5].awayTeam}`;
+    document.querySelector("#teams-nine-court-a").innerHTML = `${upcomingGames[6].homeTeam} vs. ${upcomingGames[6].awayTeam}`;
+    document.querySelector("#teams-nine-court-b").innerHTML = `${upcomingGames[7].homeTeam} vs. ${upcomingGames[7].awayTeam}`;
 
-    //document.querySelector(".date").children[0].innerHTML = "Fairy";
-    // `${(upcomingGames[0].date.split("/"))[1]}` + document.querySelector(".individual-game-day").innerHTML;
-    document.querySelector(".individual-game-month").innerHTML = `${monthNames[(upcomingGames[0].date.split("/"))[0] - 1]}`;
+    let dateClass = document.querySelectorAll(".date");
+    let courtAClass = document.querySelectorAll(".court-a");
+    let courtBClass = document.querySelectorAll(".court-b");
+    let timeSlotSixClass = document.querySelectorAll(".time-slot-six");
+    let timeSlotSevenClass = document.querySelectorAll(".time-slot-seven");
+    let timeSlotEightClass = document.querySelectorAll(".time-slot-eight");
+    let timeSlotNineClass = document.querySelectorAll(".time-slot-nine");
 
+    for (let index = 0; index < timeSlotSixClass.length; index++) {
+        timeSlotSixClass[index].innerHTML = upcomingGames[0].time;
+        timeSlotSevenClass[index].innerHTML = upcomingGames[2].time;
+        timeSlotEightClass[index].innerHTML = upcomingGames[4].time;
+        timeSlotNineClass[index].innerHTML = upcomingGames[6].time;
 
+    }
 
+    for (let index = 0; index < courtAClass.length; index++) {
+        courtAClass[index].innerHTML = `CLA - ${upcomingGames[0].court}`;
+        courtBClass[index].innerHTML = `CLA - ${upcomingGames[1].court}`;
+        console.log(courtAClass[index]);
+    }
+
+    for (let index = 0; index < dateClass.length; index++) {
+        dateClass[index].children[0].children[0].innerHTML = `${monthNames[upcomingGamesDateSplit[0] - 1]}`;
+        dateClass[index].children[0].innerHTML = `${upcomingGamesDateSplit[1]} ${dateClass[index].children[0].innerHTML}`
+    }
 
     console.log(upcomingGames);
 }
